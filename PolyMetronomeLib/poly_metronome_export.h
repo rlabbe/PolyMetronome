42d4d59b9ef1926a8a407dcb4dde3b5fceb3b1da
Roger Labbe <rlabbejr@gmail.com>
2026-06-14 17:59:41 -0700

    Change to build against static build of Qt

--- PolyMetronomeLib/poly_metronome_export.h
+++ PolyMetronomeLib/poly_metronome_export.h
@@ -2,6 +2,8 @@
 
 #if defined(POLYMETRONOMELIB_BUILDING)
 #define POLYMETRONOME_API __declspec(dllexport)
+#elif defined(POLYMETRONOMELIB_STATIC)
+#define POLYMETRONOME_API
 #else
 #define POLYMETRONOME_API __declspec(dllimport)
 #endif
